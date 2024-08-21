6c394e96618e5e244995955aa5a6801da84f84d8
saetgit <etemadian.sara.1998@gmail.com>
2024-08-21 17:35:43 +0330

    feat:edit DownloadApp

--- components/common/DownloadApp.vue
+++ components/common/DownloadApp.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-[#FFC107] flex px-[10rem] py-10">
+	<div class="bg-[#FFC107] flex px-[5rem] py-10">
 		<div class="text-[#1C1B19]">
 			<p class="text-[1.5rem] font-semibold">
 				دانلود نرم افزار بیت لند
@@ -9,18 +9,39 @@
 				به آسانی به خدمات بیت لند دسترسی داشته باشید.
 			</p>
 			<div class="flex justify-between mb-4">
-				<img src="/images/svg/app-store.svg">
-				<img src="/images/svg/google-play.svg">
-				<img src="/images/svg/windows.svg">
+				<img
+					src="/images/svg/app-store.svg"
+					alt="app-store"
+				>
+				<img
+					src="/images/svg/google-play.svg"
+					alt="google-play"
+				>
+				<img
+					src="/images/svg/windows.svg"
+					alt="windows"
+				>
 			</div>
 			<div class="flex justify-between">
-				<img src="/images/svg/app-store.svg">
-				<img src="/images/svg/google-play.svg">
-				<img src="/images/svg/windows.svg">
+				<img
+					src="/images/svg/app-store.svg"
+					alt="app-store"
+				>
+				<img
+					src="/images/svg/google-play.svg"
+					alt="google-play"
+				>
+				<img
+					src="/images/svg/windows.svg"
+					alt="windows"
+				>
 			</div>
 		</div>
 		<div>
-			<img src="/images/svg/icon-download-app.svg">
+			<img
+				src="/images/svg/icon-download-app.svg"
+				alt="icon-download-app"
+			>
 		</div>
 	</div>
 </template>
