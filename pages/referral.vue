7d643a6997625bf018e66b7c61e811f12fbfca8a
saetgit <etemadian.sara.1998@gmail.com>
2024-08-21 16:20:00 +0330

    feat:add referral page

--- pages/referral.vue
+++ pages/referral.vue
@@ -1,10 +1,94 @@
 <template>
 	<div>
-		<h1 class="text-center">
-			referral
-		</h1>
+		<div class="w-[90.094rem] h-[38.688rem] relative">
+			<img src="/images/svg/invite.svg">
+			<div class="absolute top-[12.938rem] left-[52rem]">
+				<h1 class="text-[#FFC107] text-[4rem] font-extrabold">
+					دعوت از دوستان
+				</h1>
+				<div
+					class="p-[0.625rem] bg-[#2B2B2BC2] text-white w-[30rem] h-[7.875rem] opacity-75"
+				>
+					<p>
+						بیت لند تنها پلتفرم درآمدزایی در ایران با امکان کسب درآمد تا ۵ سطح
+						زیر مجموعه <br>
+						شما در بیت لند علاوه بر معاملات خود، میتوانید به عنوان یکی از اعضای
+						بیت لند از بیت لند کسب درآمد داشته باشید. پس نباید وقتو تلف کنید !
+					</p>
+				</div>
+			</div>
+			<div
+				class="absolute top-[35rem] w-[46.125rem] bg-[#181818] py-[2.5rem] px-[2rem] transform -translate-x-1/2 "
+			>
+				<p class="text-[#FFC107] text-[1.5rem] font-extrabold pb-4">
+					چگونه شروع کنم؟
+				</p>
+				<ul class="list-disc text-[1rem] custom-list px-4 pb-4">
+					<li class="pb-3">
+						<!-- <div class="w-2 h-2 bg-[#2196F3] rounded-full"></div> -->
+						<b>ثبت نام در بیت لند !</b><br>
+						<p>
+							با ثبت نام در وبسایت بیت لند، امکان دعوت از طریق لینک برای شما
+							فعال میشود.
+						</p>
+					</li>
+					<li class="pb-3">
+						<b>دعوت دوستان</b><br>
+						<p>
+							شما با اشتراک گذاری لینک دعوت خود با دوستان میتوانید به چرخه ما
+							بپیوندید.
+						</p>
+					</li>
+					<li class="pb-3">
+						<b>دریافت کمیسیون!</b><br>
+						<p>
+							حال شما میتوانید ۳۰ تا ۴۵ درصد از زیرمجموعه های شاخه اول و ۵ درصد
+							بصورت ثابت تا شاخه پنجم درامد کسب کنید.
+						</p>
+					</li>
+				</ul>
+				<div class="flex text-[#FFC107] font-extrabold text-[1rem] justify-center">
+					<p class="pl-2">
+						شروع کنید
+					</p>
+					<img src="/images/svg/arrow-left.svg">
+				</div>
+			</div>
+			<div class="mt-[30rem] flex justify-between px-[10rem] mb-10">
+				<div>
+					<img src="/images/svg/platform.svg">
+				</div>
+				<div class="pr-8">
+					<p class="text-[2.5rem] font-extrabold pb-8">
+						جدول درآمدزایی پلتفرم بیت لند
+					</p>
+					<ul class="list-disc text-[1rem]">
+						<li>
+							کارمزد شما از سطح اول زیر مجموعه های خود از ۳۰ درصد تا ۴۵ درصد نسبت به سطح کسب درآمدی شما متغیر می باشد.
+						</li><br>
+						<li>
+							کارمزد شما از سطح دوم ۵ درصد‌‌، سطح سوم ۲/۵ درصد و سطح چهارم ۱/۶ درصد میباشد که ثابت بوده و قابل تغییر نیست.
+						</li><br>
+						<li>
+							تمامی کارمزد های شما در ساعات مشخصی از روز محاسبه و به حساب کاربری شما واریز می گردد.
+						</li><br>
+						<li>
+							صرافی بیت لند آماده همکاری با اشخاص مطرح، شرکت ها و سازمان ها با تسهیلات و خدمات ویژه می باشد.
+						</li>
+					</ul>
+				</div>
+				<div />
+			</div>
+			<CommonDownloadApp />
+		</div>
 	</div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
+
+<style scoped>
+.custom-list li::marker {
+    color: #007BFF;
+    font-size: 2rem;
+}
+</style>
