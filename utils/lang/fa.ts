87fda89c8bc31e5976fc40870b973d523b79d35d
saetgit <etemadian.sara.1998@gmail.com>
2024-08-22 17:21:16 +0330

    feat:privacy page

--- utils/lang/fa.ts
+++ utils/lang/fa.ts
@@ -13,6 +13,13 @@ const persian = {
 	haveReferralCode: 'کد معرف دارید؟',
 	termsAndConditions: 'قوانین و شرایط استفاده',
 	agreeWithConditions: ' از بیت لند را خوانده‌ام و موافقم.',
+	termsRules: 'شرایط و قوانین',
+	generalRules: 'قوانین کلی ',
+	authentication: ' احراز هویت',
+	rate: 'نرخ',
+	wallet: 'کیف پول',
+	provideServices: 'نحوه ارائه خدمات',
+
 };
 
 export default persian;
