69c75070ada7acaa27097572e5233d5b5be18990
Hossein <hossein.bajan@gmail.com>
2024-08-26 08:24:23 +0330

    feat: add a component to use in website headers

---
+++ components/pages/ImageCover.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="relative">
+		<NuxtImg
+			:src="`/images/bg/${isDark? 'dark.webp': 'light.webp'}`"
+			alt="bg-cover"
+			class="w-full"
+		/>
+		<div class="absolute top-0 left-1/2 transform -translate-x-1/2 w-full max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+const isDark = computed({
+	get() {
+		return colorMode.value === 'dark';
+	},
+	set() {
+		colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+	},
+});
+</script>
