40fd515de66abe594ffcc7f425ec4d451d1da140
saetgit <etemadian.sara.1998@gmail.com>
2024-09-09 10:34:04 +0330

    feat:add user-level in profile

---
+++ components/pages/Site/Account/UserLevel/Chart.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="relative w-72 h-72  rounded-full">
+		<ClientOnly>
+			<VChart
+				:option="chartOptions"
+				style="height: 100%; width: 100%;"
+			/>
+			<div class="absolute inset-0 flex flex-col items-center justify-center">
+				<span class="text-yellow-400 text-lg"> {{ useNumber('876,321') }}{{ $t('dollar') }}</span>
+				<div class="flex items-center justify-center mt-2 bg-gray-800 rounded-full px-3 py-1">
+					<span class="text-white text-lg">سطح ماهی</span>
+					<NuxtImg
+						src="/images/delete/fish.png"
+						alt="Fish Icon"
+						class="w-6 h-6 ml-2"
+					/>
+				</div>
+			</div>
+		</ClientOnly>
+	</div>
+</template>
+
+<script setup>
+import { useNumber } from '~/composables/useNumber';
+
+const chartOptions = computed(() => ({
+	series: [
+		{
+			type: 'pie',
+			radius: ['50%', '0%'],
+			avoidLabelOverlap: false,
+			label: {
+				show: false,
+			},
+			emphasis: {
+				label: {
+					show: false,
+				},
+			},
+			labelLine: {
+				show: false,
+			},
+			data: [
+				{ value: 33, name: '100', itemStyle: { color: '#FFCC00' } },
+				{ value: 67, name: '1000', itemStyle: { color: '#444444' } },
+			],
+		},
+	],
+	backgroundColor: '#000000',
+	graphic: [
+		{
+			type: 'text',
+			left: 'center',
+			top: 'center',
+			style: {
+				text: '876,321 دلار',
+				textAlign: 'center',
+				fill: '#FFD700',
+				fontSize: 18,
+				fontWeight: 'bold',
+			},
+		},
+	],
+}));
+</script>
---
+++ components/pages/Site/Account/UserLevel/TableRow.vue
@@ -0,0 +1,52 @@
+<template>
+	<tr class=" flex py-3 text-center border-b border-b-primary-gray-light dark:border-b-primary-gray-dark">
+		<td class="text-sm font-normal py-2 text-primary-yellow-light dark:text-primary-yellow-dark">
+			<NuxtImg
+				src="/images/delete/fish.png"
+				alt="icon"
+				class="w-16 h-16"
+			/>
+			اسکلت
+		</td>
+		<td class="text-sm font-normal py-2  text-primary-yellow-light dark:text-primary-yellow-dark">
+			بدون معامله کافی
+		</td>
+		<td class="text-sm font-normal py-2  text-primary-yellow-light dark:text-primary-yellow-dark">
+			جایزه ای ندارد
+		</td>
+		<td class="text-sm font-normal py-2">
+			<IconClose class="text-xs" />
+		</td>
+	</tr>
+	<!-- <tr class="py-3 text-center border-b border-b-primary-gray-light dark:border-b-primary-gray-dark">
+			<td class="text-sm font-normal py-2">
+				<NuxtImg
+					src="/images/delete/fish.png"
+					alt="icon"
+					class="w-16 h-16"
+				/>
+				میگو
+			</td>
+			<td class="text-sm font-normal py-2">
+				حداقل حجم معاملات 1000 دلار
+			</td>
+			<td class="text-sm font-normal py-2">
+				صد هزار شیبا
+			</td>
+			<td class="text-sm font-normal py-2">
+				<NuxtImg
+					src="/images/delete/fish.png"
+					alt="icon"
+					class="w-16 h-16"
+				/>
+			</td>
+		</tr> -->
+</template>
+
+<script setup lang="ts">
+import IconClose from '~/assets/svg-icons/close.svg';
+</script>
+
+<style scoped>
+
+</style>
