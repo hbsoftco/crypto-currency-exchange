b0568c0e8b36cb670a003274d1a0aa78b582137e
saetgit <etemadian.sara.1998@gmail.com>
2024-09-10 17:01:51 +0330

    feat:award in profile

--- pages/account/rewards.vue
+++ pages/account/rewards.vue
@@ -1,10 +1,30 @@
 <template>
 	<div>
-		Page: /account/rewards
+		<section>
+			<div class="my-6">
+				<UiTitleWithBack
+					:title="$t('awards')"
+					:back-btn="false"
+				/>
+			</div>
+		</section>
+
+		<section>
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+				<AwardBox />
+				<AwardBox />
+				<AwardBox />
+				<AwardBox />
+				<AwardBox />
+				<AwardBox />
+			</div>
+		</section>
 	</div>
 </template>
 
 <script setup lang="ts">
+import AwardBox from '~/components/pages/Site/Account/Award/AwardBox.vue';
+
 definePageMeta({
 	layout: 'account',
 });
