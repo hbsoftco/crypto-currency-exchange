4af3ab022e9498945e20d088ef2d95ba76196ff6
saetgit <etemadian.sara.1998@gmail.com>
2024-09-11 16:07:29 +0330

    feat:addd award in profile

--- components/pages/Site/Account/Award/AwardBox.vue
+++ components/pages/Site/Account/Award/AwardBox.vue
@@ -1,46 +1,109 @@
 <template>
-	<div class="relative bg-hover-light dark:bg-hover-dark px-4">
-		<div class="absolute left-0 w-full mx-auto">
-			<div class="flex flex-row-reverse items-center space-x-1 bg-secondary-gray-light dark:bg-secondary-gray-dark rounded-b-xl">
-				<div
-					class=" bg-primary-yellow-light dark:bg-primary-yellow-dark h-2 rounded-bl-xl"
-					style="width: 30%;"
-				>
-					<span class="text-left text-primary-yellow-light dark:text-primary-yellow-dark py-1">{{ useNumber('25') }}%</span>
+	<div>
+		<AwardDetail
+			v-if="showDetail"
+			@close="closeDetail"
+		/>
+
+		<AwardShow
+			v-if="showAward"
+			@close="closeAward"
+		/>
+
+		<div
+			class="relative bg-hover-light dark:bg-hover-dark px-4"
+		>
+			<div class="absolute left-0 w-full px-4">
+				<div class="flex flex-row-reverse items-center space-x-1 bg-secondary-gray-light dark:bg-secondary-gray-dark rounded-b-xl">
+					<div
+						class=" bg-primary-yellow-light dark:bg-primary-yellow-dark h-2 rounded-bl-xl"
+						style="width: 30%;"
+					/>
 				</div>
 			</div>
-		</div>
-		<div class="flex item-center py-3">
-			<NuxtImg
-				src="/images/delete/award.png"
-				alt="award"
-				class="w-12 h-12 rounded-full ml-2"
-			/>
-			<span class="text-base font-bold">{{ $t('authentication') }}</span>
-		</div>
-		<div
-			class="pb-6 flex item-center flex-row-reverse justify-start text-sm font-normal"
-		>
-			<NuxtImg
-				src="/images/delete/bitcoin.png"
-				alt="award"
-				class="w-4 h-4 rounded-full mr-2"
-			/>
-			<span class="mr-1">{{ useNumber('۵۰۰.۰۰۰.۰۰۰') }}</span><span>babydoge</span>
-		</div>
-		<div class="text-left pb-4">
-			<UButton
-				size="lg"
-				class=" bg-secondary-gray-light dark:bg-secondary-gray-dark px-0 md:px-9"
+			<span class="absolute left-2 top-2 px-2 text-primary-yellow-light dark:text-primary-yellow-dark py-1">{{ useNumber('25') }}%</span>
+
+			<div class="flex item-center py-4">
+				<NuxtImg
+					src="/images/delete/award.png"
+					alt="award"
+					class="w-12 h-12 rounded-full ml-2"
+				/>
+				<div class="">
+					<h4 class="text-base font-bold">
+						{{ $t('authentication') }}
+					</h4>
+					<div class="flex">
+						<span class="text-sm font-normal ml-1">01:23:14:56</span>
+						<IconClock class="text-base" />
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="pb-6 flex item-center flex-row-reverse justify-start text-sm font-normal"
 			>
-				<span class="text-sm font-bold text-dark dark:text-white">{{
-					$t("moreDetail")
-				}}</span>
-			</UButton>
+				<NuxtImg
+					src="/images/delete/bitcoin.png"
+					alt="award"
+					class="w-4 h-4 rounded-full mr-2"
+				/>
+				<span class="mr-1">{{ useNumber('۵۰۰.۰۰۰.۰۰۰') }}</span><span>babydoge</span>
+			</div>
+
+			<div class="flex justify-between flex-row-reverse pb-4">
+				<UButton
+					size="lg"
+					class=" bg-secondary-gray-light dark:bg-secondary-gray-dark px-2 md:px-9"
+					@click="openDetail"
+				>
+					<span class="text-sm font-bold text-dark dark:text-white">{{
+						$t("moreDetail")
+					}}</span>
+				</UButton>
+
+				<ULink @click.prevent="openAward">
+					<div class="relative">
+						<IconAwards class=" text-3xl text-primary-yellow-light dark:text-primary-yellow-dark" />
+					</div>
+					<div class="absolute bottom-4 right-2 flex items-center w-5 h-5 bg-accent-red rounded-full justify-center">
+						<p class="text-xs font-bold">
+							{{ useNumber('2') }}
+						</p>
+					</div>
+				</ULink>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import { useNumber } from '~/composables/useNumber';
+import IconAwards from '~/assets/svg-icons/menu/quick-menu/awards.svg';
+import IconClock from '~/assets/svg-icons/profile/clock.svg';
+import AwardShow from '~/components/pages/Site/Account/Award/AwardShow.vue';
+import AwardDetail from '~/components/pages/Site/Account/Award/AwardDetail.vue';
+
+const showDetail = ref(false);
+const showAward = ref(false);
+
+const openDetail = () => {
+	showDetail.value = true;
+};
+
+const closeDetail = () => {
+	showDetail.value = false;
+};
+
+const openAward = () => {
+	showAward.value = true;
+};
+
+const closeAward = () => {
+	showAward.value = false;
+};
 </script>
+
+  <style scoped></style>
---
+++ components/pages/Site/Account/Award/AwardDetail.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<UModal
+			v-model="isOpen"
+			fullscreen
+		>
+			<div
+				class="h-full flex flex-col items-center justify-center bg-secondary-gray-light dark:bg-secondary-gray-dark"
+			>
+				<div
+					class="flex flex-col justify-center items-center bg-background-light dark:bg-background-dark px-1 md:px-20 py-6 md:py-8 mb-0 md:mb-6 my-32 md:my-0"
+				>
+					<div class="block md:hidden w-full">
+						<div class="flex justify-between items-center">
+							<h3 class="text-base font-medium">
+								{{ $t("yourAward") }}
+							</h3>
+							<IconClose
+								class="text-3xl"
+								@click="isOpen = false"
+							/>
+						</div>
+					</div>
+					<h4
+						class="text-center text-4xl font-black text-primary-yellow-light dark:text-primary-yellow-dark"
+					>
+						{{ $t("congratulations") }}
+					</h4>
+					<NuxtImg
+						src="/images/profile/Award-modal.png"
+						alt="award"
+						class="w-full md:w-80 h-80 text-center"
+					/>
+					<p class="text-base font-bold text-white text-center">
+						{{ $t("awardText") }}
+					</p>
+				</div>
+				<IconClose
+					class="text-4xl hidden md:block"
+					@click="isOpen = false"
+				/>
+			</div>
+		</UModal>
+	</div>
+</template>
+
+<script setup lang="ts">
+import IconClose from '~/assets/svg-icons/close.svg';
+
+const isOpen = ref(true);
+</script>
