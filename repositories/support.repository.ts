f2159c2968b6206e94dcb00d4ea0317dcd052c41
saetgit <etemadian.sara.1998@gmail.com>
2024-10-02 14:30:21 +0330

    feat:add vertification dynamic

---
+++ repositories/support.repository.ts
@@ -0,0 +1,20 @@
+import type { NitroFetchRequest, $Fetch } from 'nitropack';
+
+import type { CustomNitroFetchOptions } from '~/types/custom-nitro-fetch-options.types';
+import type { SocialNetListResponse } from '~/types/response/support.types';
+
+type SupportRepository = {
+	getSocialNetList: () => Promise<SocialNetListResponse>;
+};
+
+export const supportRepository = (fetch: $Fetch<unknown, NitroFetchRequest>): SupportRepository => ({
+	async getSocialNetList(): Promise<SocialNetListResponse> {
+		const url = ' /v1/routine/support/social_net_list';
+		const response = await fetch<SocialNetListResponse>(`${url}`, {
+			noAuth: false,
+			apiName: url,
+		} as CustomNitroFetchOptions);
+
+		return response;
+	},
+});
---
+++ types/response/support.types.ts
@@ -0,0 +1,10 @@
+import type { KeyValue } from '../base.types';
+
+type SocialNetListResponse = {
+	statusCode: number;
+	result: KeyValue[];
+};
+
+export type {
+	SocialNetListResponse,
+};
