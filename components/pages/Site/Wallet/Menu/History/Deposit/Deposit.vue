10ef5d0339d274c920ff9bc157fb75b2078e1107
Hossein <hossein.bajan@gmail.com>
2024-10-06 22:29:26 +0330

    feat:edit history in deposit

--- components/pages/Site/Wallet/Menu/History/Deposit/Deposit.vue
+++ components/pages/Site/Wallet/Menu/History/Deposit/Deposit.vue
@@ -1,9 +1,5 @@
 <template>
 	<div>
-		<!-- <DepositDetails
-			v-if="showDetail"
-			@close="closeDetail"
-		/> -->
 		<DepositDetailToman
 			v-if="showDetail"
 			@close="closeDetail"
@@ -11,8 +7,8 @@
 		<div class="block md:flex items-center my-2">
 			<div class="ml-6 my-1 w-44">
 				<USelectMenu
-					v-model="orderTypeFilter"
-					:options="orderTypeItems"
+					v-model="cryptoTypeFilter"
+					:options="cryptoTypeItems"
 					:placeholder="$t('orderType')"
 					option-attribute="value"
 					:ui="{
@@ -59,7 +55,6 @@
 						},
 					}"
 				/>
-
 				<DatePicker
 					v-model="fromDate"
 					color="#FFC107"
@@ -87,7 +82,6 @@
 						},
 					}"
 				/>
-
 				<DatePicker
 					v-model="toDate"
 					display-format="jYYYY/jMM/jDD"
@@ -105,7 +99,7 @@
 			</div>
 			<UButton
 				class="flex justify-center px-8 text-sm font-normal text-black dark:text-white hover:text-hover-light dark:hover:text-hover-light bg-hover-light dark:bg-hover-dark shadow-none border border-primary-gray-light dark:border-primary-gray-dark"
-				@click="applyFilter"
+				@click="applyFilters"
 			>
 				{{ $t("search") }}
 			</UButton>
@@ -163,7 +157,7 @@
 						<td class="text-nowrap text-xs font-normal py-2">
 							<UButton
 								size="lg"
-								class="text-base font-medium px-4 py-2 text-center bg-transparent-light dark:bg-transparency-dark text-primary-yellow-light dark:text-primary-yellow-dark border border-primary-yellow-light dark:border-primary-yellow-dark hover:text-text-light hover:dark:text-text-light "
+								class="text-base font-medium px-4 py-2 text-center bg-transparent-light dark:bg-transparency-dark text-primary-yellow-light dark:text-primary-yellow-dark border border-primary-yellow-light dark:border-primary-yellow-dark hover:text-text-light hover:dark:text-text-light"
 								to=""
 								@click="openDetail"
 							>
@@ -196,34 +190,31 @@
 <script setup lang="ts">
 import { useNumber } from '~/composables/useNumber';
 import { formatDateToIranTime } from '~/utils/date-time.js';
-// import DepositDetails from '~/components/pages/Site/Wallet/Menu/History/Deposit/DepositDetails.vue';
 import DepositDetailToman from '~/components/pages/Site/Wallet/Menu/History/Deposit/DepositDetailToman.vue';
 import { depositRepository } from '~/repositories/deposit.repository';
-import type { GetDepositParams, KeyValue } from '~/types/base.types';
+import type { KeyValue } from '~/types/base.types';
 import type { Deposit } from '~/types/response/deposit.types';
 import { DepositType } from '~/utils/enums/deposit.enum';
 
-const toDate = ref();
-const fromDate = ref();
-const value = ref('');
+const cryptoTypeItems = ref<KeyValue[]>([
+	{
+		key: DepositType.ANY,
+		value: useT(DepositType.ANY),
+	},
+	// Add more items here...
+]);
+
 const depositTypeItems = ref<KeyValue[]>([
 	{
 		key: DepositType.ANY,
 		value: useT(DepositType.ANY),
 	},
-	{
-		key: DepositType.ANY,
-		value: useT(DepositType.ANY),
-	},
-	{
-		key: DepositType.ANY,
-		value: useT(DepositType.ANY),
-	},
-	{
-		key: DepositType.ANY,
-		value: useT(DepositType.ANY),
-	},
+	// Add more items here...
 ]);
+const fromDate = ref();
+const toDate = ref();
+
+const cryptoTypeFilter = ref<KeyValue>();
 const depositTypeFilter = ref<KeyValue>();
 
 const { $api } = useNuxtApp();
@@ -240,23 +231,33 @@ const params = ref({
 	currencyType: '',
 	allBlockchains: '',
 	invoiceNumber: '',
-
 });
+
+const totalCount = ref(0);
 const response = await depositRepo.getDeposit(params.value);
-
 const depositList = ref<Deposit[]>(response.result.rows);
-console.log('--------------------------->', depositList);
 
-const applyFilter = async (event: OrderFiltersType) => {
-	params.value.type = event.orderType;
-	params.value.currencyType = event.orderSide;
-	params.value.allBlockchains = event.symbol;
-	params.value.from = event.from;
-	params.value.to = event.to;
-	params.value.invoiceNumber = event.symbol;
-
-	await fetchOrderList();
+const loadDeposits = async () => {
+	try {
+		const response = await depositRepo.getDeposit(params.value);
+		depositList.value = response.result.rows;
+		totalCount.value = response.result.totalCount;
+	}
+	catch (error) {
+		console.error('Error fetching trades:', error);
+	}
 };
+
+const applyFilters = async () => {
+	params.value.currencyType = cryptoTypeFilter.value ? cryptoTypeFilter.value.key : '';
+	params.value.type = depositTypeFilter.value ? depositTypeFilter.value.key : '';
+	await loadDeposits();
+};
+
+onMounted(async () => {
+	await loadDeposits();
+});
+
 const showDetail = ref(false);
 const openDetail = () => {
 	showDetail.value = true;
@@ -265,10 +266,9 @@ const openDetail = () => {
 const closeDetail = () => {
 	showDetail.value = false;
 };
-const totalCount = ref(0);
 
 const onPageChange = async (newPage: number) => {
-	params.value.pageNumber = String(newPage);
-	await depositList;
+	params.value.pageNumber = newPage.toString();
+	await loadDeposits();
 };
 </script>
