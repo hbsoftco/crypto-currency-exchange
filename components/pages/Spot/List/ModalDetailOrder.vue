a4e00344eee57d9ffe3a5f1eba7f8338786d9774
saetgit <etemadian.sara.1998@gmail.com>
2024-10-09 12:12:23 +0330

    feat:modal in props

--- components/pages/Spot/List/ModalDetailOrder.vue
+++ components/pages/Spot/List/ModalDetailOrder.vue
@@ -5,20 +5,21 @@
 			fullscreen
 		>
 			<div
-				class="h-full flex flex-col items-center justify-center "
+				class="h-full flex flex-col items-center justify-center overflow-y-scroll"
 			>
+				<h1>hossein {{ order?.assetTypeName }}</h1>
 				<div
-					class="overflow-y-scroll w-full md:w-[45rem] flex flex-col justify-center items-center text-center rounded-md bg-background-light dark:bg-background-dark px-2 md:px-14 pt-[40rem] md:pt-96 mt-20"
+					class=" w-full md:w-[45rem] flex flex-col justify-center items-center text-center rounded-md bg-background-light dark:bg-background-dark px-2 md:px-14 py-6 md:py-8 mt-20"
 				>
-					<div class="flex md:hidden justify-between py-3 items-center ">
+					<div class=" w-full block md:hidden justify-between py-3 items-center ">
 						<UiTitleWithBack
 							:title="$t('orderDetails')"
 							:back-btn="true"
 						/>
 					</div>
 					<span class="text-xl font-bold hidden md:block">{{ $t('orderDetails') }}</span>
-					<section>
-						<div class="flex justify-between px-1 md:px-16 my-4">
+					<section class="w-full ">
+						<div class="flex justify-between px-1 my-4">
 							<div class="flex">
 								<NuxtImg
 									src="/images/delete/bitcoin.png"
@@ -39,13 +40,13 @@
 						</div>
 					</section>
 
-					<section>
+					<section class="w-full ">
 						<div class="mb-12 grid grid-cols-2 md:grid-cols-3 gap-4">
 							<div class="flex m-2">
 								<IconType class="text-2xl" />
 								<div class="mr-2 flex flex-col text-right">
 									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('type') }}:</span>
-									<span class="text-sm font-bold">بازار</span>
+									<span class="text-sm font-bold">{{ $t(order?.apiKey) }}</span>
 								</div>
 							</div>
 							<div class="flex m-2">
@@ -106,108 +107,6 @@
 							</div>
 						</div>
 					</section>
-
-					<section>
-						<div class="flex justify-center mb-4 ">
-							<h3 class="text-2xl font-bold">
-								{{ $t('transactionDetails') }}
-							</h3>
-						</div>
-						<div class="flex justify-start py-1 border-b border-primary-gray-light dark:border-primary-gray-dark">
-							<span class="text-sm font-bold ">{{ $t('firstTransaction') }}</span>
-						</div>
-						<div class="mb-4 grid grid-cols-2 md:grid-cols-3 gap-4">
-							<div class="flex m-2">
-								<IconDate class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('date') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber(' ۰۲/۰۳/۲۴ - ۲۳:۴۵:۵۸') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconSalary class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('marketPrice') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۳۵۶۸۹۳۵۶') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconCoin class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('fee') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۳۸۶۳۸۵ USDT') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconCategory class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('status') }}:</span>
-									<span class="text-sm font-bold">پایان یافته</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconAnchor class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('amount') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۲۳۲۳۸۴۶۳۲۸۶۳۲۸۳۸۲۴') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconRecepie class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('totalAmount') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۲۳۷۵۸ TMN') }}</span>
-								</div>
-							</div>
-						</div>
-						<div class="flex justify-start py-1 border-b border-primary-gray-light dark:border-primary-gray-dark">
-							<span class="text-sm font-bold ">{{ $t('secondTransaction') }}</span>
-						</div>
-						<div class="mb-4 grid grid-cols-2 md:grid-cols-3 gap-4">
-							<div class="flex m-2">
-								<IconDate class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('date') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber(' ۰۲/۰۳/۲۴ - ۲۳:۴۵:۵۸') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconSalary class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('marketPrice') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۳۵۶۸۹۳۵۶') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconCoin class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('fee') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۳۸۶۳۸۵ USDT') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconCategory class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('status') }}:</span>
-									<span class="text-sm font-bold">پایان یافته</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconAnchor class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('amount') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۲۳۲۳۸۴۶۳۲۸۶۳۲۸۳۸۲۴') }}</span>
-								</div>
-							</div>
-							<div class="flex m-2">
-								<IconRecepie class="text-2xl" />
-								<div class="mr-2 flex flex-col text-right">
-									<span class="text-xs font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('totalAmount') }}:</span>
-									<span class="text-sm font-bold">{{ useNumber('۲۳۷۵۸ TMN') }}</span>
-								</div>
-							</div>
-						</div>
-					</section>
 				</div>
 				<div class="mt-6">
 					<IconClose
@@ -229,17 +128,23 @@ import IconAnchor from '~/assets/svg-icons/spot/anchor.svg';
 import IconAnalytics from '~/assets/svg-icons/spot/analytics.svg';
 import IconCoin from '~/assets/svg-icons/spot/coin.svg';
 import IconSalary from '~/assets/svg-icons/spot/salary.svg';
-import IconDate from '~/assets/svg-icons/menu/quick-menu/transaction-history.svg';
 import { useNumber } from '~/composables/useNumber';
+import type { Order } from '~/types/response/spot.types';
 
-const isOpen = ref(true);
+const props = defineProps<{ order: Order }>();
+const isOpen = ref<boolean>(false);
 interface EmitDefinition {
 	(event: 'close', value: boolean): void;
 }
+console.log(props.order);
 
 const emit = defineEmits<EmitDefinition>();
 
 const closeModal = async (value: boolean) => {
 	emit('close', value);
 };
+
+onMounted(() => {
+	isOpen.value = true;
+});
 </script>
