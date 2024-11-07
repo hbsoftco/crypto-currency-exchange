e4003d15172e0150eddd93cd4d719fa5be580f22
saetgit <etemadian.sara.1998@gmail.com>
2024-11-07 10:59:09 +0330

    fix bug:detail market hover

--- components/pages/Coins/SidebarMarketState.vue
+++ components/pages/Coins/SidebarMarketState.vue
@@ -4,7 +4,7 @@
 			<li
 				v-for="market in markets"
 				:key="`market-state-${market.id}`"
-				class="flex justify-between items-center my-3 mx-2"
+				class="flex justify-between items-center px-2 py-3 hover:bg-primary-gray-light hover:dark:bg-primary-gray-dark rounded duration-200 transition-all"
 			>
 				<ULink
 					class="flex justify-between"
@@ -26,7 +26,7 @@
 				</ULink>
 				<div>
 					<div>
-						<span class="text-sm font-normal mr-1">{{ useNumber(market?.indexPrice) }}</span>
+						<span class="text-sm font-normal mr-1">{{ useNumber(priceFormat(market?.indexPrice)) }}</span>
 						<span class="text-sm font-normal text-subtle-text-light dark:text-subtle-text-dark">
 							{{ market?.quote?.cSymbol }}
 						</span>
@@ -41,6 +41,7 @@
 import { splitMarket } from '~/utils/split-market';
 import { useNumber } from '~/composables/useNumber';
 import type { MarketState } from '~/types/definitions/market.types';
+import { priceFormat } from '~/utils/price-format';
 
 interface Props {
 	markets: MarketState[];
