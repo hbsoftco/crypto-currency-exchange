34741c52a7f26c454737c0a0262a2218cd2cd964
saetgit <etemadian.sara.1998@gmail.com>
2024-11-09 10:22:28 +0330

    feat:help buy

--- pages/help-buy/index.vue
+++ pages/help-buy/index.vue
@@ -7,7 +7,11 @@
 						<h1 class="text-4xl font-black">
 							{{ $t('helpBuyList') }}
 						</h1>
-						<span class="py-4 text-sm font-normal text-subtle-text-light dark:text-subtle-text-dark">{{ $t('helpBuyListTitle') }}</span>
+						<span
+							class="py-4 text-sm font-normal text-subtle-text-light dark:text-subtle-text-dark"
+						>
+							{{ $t('helpBuyListTitle') }}
+						</span>
 					</div>
 					<div>
 						<img
@@ -19,8 +23,8 @@
 				</div>
 				<div class="w-full md:w-96">
 					<SearchCrypto
-						id="phoneOrEmail"
-						v-model="phoneOrEmail"
+						id="searchMenu"
+						v-model="searchMenu"
 						type="text"
 						input-class="text-right"
 						label="search"
@@ -34,7 +38,7 @@
 			<UContainer>
 				<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
 					<ULink
-						v-for="(item, index) in buyList"
+						v-for="(item, index) in filteredTreeList"
 						:key="index"
 						:to="`help-buy/${item.id}`"
 						class="flex justify-between items-center py-4 border-b border-primary-gray-light dark:border-primary-gray-dark"
@@ -54,9 +58,9 @@
 				</div>
 				<div class="flex justify-center py-4">
 					<UPagination
-						:model-value="Number(params.pageNumber)"
+						:model-value="Number(buyingGuideStore.params.pageNumber)"
 						:page-count="20"
-						:total="totalCount"
+						:total="buyingGuideStore.totalCount"
 						:max="6"
 						size="xl"
 						ul-class="flex space-x-2 bg-blue-500 border-none"
@@ -65,7 +69,7 @@
 						button-class-inactive="bg-green-700 hover:bg-gray-600"
 						button-class-active="bg-blue-500"
 						class="my-14"
-						@update:model-value="onPageChange"
+						@update:model-value="buyingGuideStore.onPageChange"
 					/>
 				</div>
 			</UContainer>
@@ -76,83 +80,19 @@
 <script setup lang="ts">
 import SearchCrypto from '~/components/forms/SearchCrypto.vue';
 import IconArrowLeft from '~/assets/svg-icons/menu/arrow-left.svg';
-import { helpRepository } from '~/repositories/help.repository';
-import type { GetHowBuyListParams } from '~/types/base.types';
-import type { BuyItem } from '~/types/response/help.types';
-import type { CurrencyBriefItem } from '~/types/response/brief-list.types';
-import { Language } from '~/utils/enums/language.enum';
 
-const baseDataStore = useBaseDataStore();
-const phoneOrEmail = ref('');
-
-const { $api } = useNuxtApp();
-const helpRepo = helpRepository($api);
-
-const totalCount = ref(0);
-
-const params = ref<GetHowBuyListParams>({
-	languageId: '',
-	tagId: '',
-	searchStatement: '',
-	group: '',
-	pageNumber: '1',
-	pageSize: '20',
-});
-
-const response = await helpRepo.getBuyList(params.value);
-const buyList = ref<BuyItem[]>(response.result.rows);
-const isLoading = ref<boolean>(false);
-
-console.log('buyList------------------------', buyList);
-const findCurrencyById = (id: number): CurrencyBriefItem | null => {
-	let start = 0;
-	let end = baseDataStore.currencyBriefItems.length - 1;
-
-	while (start <= end) {
-		const mid = Math.floor((start + end) / 2);
-		const currentItem = baseDataStore.currencyBriefItems[mid];
-
-		if (currentItem.id === id) {
-			return currentItem;
-		}
-		else if (currentItem.id < id) {
-			start = mid + 1;
-		}
-		else {
-			end = mid - 1;
-		}
-	}
-
-	return null;
-};
-const fetchBuyList = async () => {
-	try {
-		const { result } = await helpRepo.getBuyList(params.value);
-		await baseDataStore.fetchCurrencyBriefItems(Language.PERSIAN);
-
-		totalCount.value = result.totalCount;
-		buyList.value = result.rows;
-		buyList.value = buyList.value.map((buy) => {
-			const currency = findCurrencyById(buy.cid);
-			return {
-				...buy,
-				currency: currency ? currency : null,
-			};
-		});
-
-		isLoading.value = false;
-	}
-	catch (error) {
-		console.error('Error fetching trades:', error);
-	}
-};
+const buyingGuideStore = useBuyingGuideStore();
 
 onMounted(async () => {
-	await fetchBuyList();
+	await buyingGuideStore.fetchBuyList();
 });
 
-const onPageChange = async (newPage: number) => {
-	params.value.pageNumber = String(newPage);
-	await fetchBuyList();
-};
+const searchMenu = ref('');
+const filteredTreeList = computed(() => {
+	if (!searchMenu.value) return buyingGuideStore.buyList;
+
+	return buyingGuideStore.buyList.filter((item) =>
+		item.info?.header?.toLowerCase().includes(searchMenu.value.toLowerCase()),
+	);
+});
 </script>
