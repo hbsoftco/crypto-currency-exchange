d71fa50473e50ca763ceaff8b9ac59be35c4ae60
Hossein <hossein.bajan@gmail.com>
2024-12-17 23:34:19 +0330

    update chart url

--- components/pages/Spot/OrderFlow/BitlandChart.vue
+++ components/pages/Spot/OrderFlow/BitlandChart.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-full h-[26rem]">
 		<iframe
-			src="https://tv.hosseinbajan.ir/datafeed/1/spot/BTCUSDT?templateId=dark&interval=1hour"
+			src="https://tv.hosseinbajan.ir/datafeed/spot/BTCUSDT?templateId=dark&interval=1hour"
 			class="w-full h-full"
 			frameborder="0"
 			allowfullscreen
