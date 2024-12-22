be023f711f498c9ba96a864d49d444e1aecbf52e
Hossein <hossein.bajan@gmail.com>
2024-12-22 12:06:38 +0330

    fix background in firfox

--- components/pages/Spot/OrderFlow/GlobalChart.vue
+++ components/pages/Spot/OrderFlow/GlobalChart.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="w-full h-[26rem]">
+		<!-- :src="`https://tv.hosseinbajan.ir/global?theme=${isDark? 'dark': 'light'}`" -->
 		<iframe
-			src="https://tv.hosseinbajan.ir/global?theme=dark"
+			:src="`http://localhost:5173/global?theme=${isDark? 'dark': 'light'}`"
 			class="w-full h-full"
 			frameborder="0"
 			allowfullscreen
@@ -10,4 +11,13 @@
 </template>
 
 <script setup lang="ts">
+const colorMode = useColorMode();
+const isDark = computed({
+	get() {
+		return colorMode.value === 'dark';
+	},
+	set() {
+		colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+	},
+});
 </script>
