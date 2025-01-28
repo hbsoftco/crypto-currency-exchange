08e6d07c79e92797f296de1c696191286d436b58
Hossein <hossein.bajan@gmail.com>
2025-01-28 11:10:08 +0330

    refactore: remove components no longer need

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,14 +1,98 @@
 <template>
-	<div>
-		<Login />
-	</div>
+	<Auth title="login">
+		<UTabs
+			:items="items"
+			@change="onChange"
+		>
+			<template #default="{ item, selected }">
+				<span
+					class="truncate"
+					:class="[selected && 'text-primary-500 dark:text-primary-400']"
+				>{{ $t(item.label) }}</span>
+			</template>
+			<template #item="{ item }">
+				<div
+					v-if="item.key === 'phoneNumber'"
+					class="space-y-3"
+				>
+					<WithMobile v-if="item.key === 'phoneNumber'" />
+				</div>
+				<div
+					v-else-if="item.key === 'email'"
+					class="space-y-3"
+				>
+					<WithEmail v-if="item.key === 'email'" />
+				</div>
+				<div
+					v-else-if="item.key === 'qrCode'"
+					class="space-y-3"
+				>
+					<WithQRCode v-if="item.key === 'qrCode'" />
+				</div>
+			</template>
+		</UTabs>
+
+		<div class="flex justify-between items-center mt-8">
+			<div>
+				<span class="ml-2 text-sm">{{ $t("noAccount") }}</span>
+				<ULink
+					to="/auth/sign-up"
+					class="text-accent-green text-sm font-bold"
+					active-class="text-green"
+					inactive-class=""
+				>
+					{{ $t('signup') }}
+				</ULink>
+			</div>
+			<div>
+				<ULink
+					to="/auth/forget-password"
+					class="text-accent-red text-sm font-bold"
+					active-class="text-green"
+					inactive-class=""
+				>
+					{{ $t('forgotPassword') }}
+				</ULink>
+			</div>
+		</div>
+	</Auth>
 </template>
 
-<script setup>
-import Login from '~/components/pages/Auth/Login/index.vue';
+<script setup lang="ts">
+import Auth from '~/components/pages/Auth/Auth.vue';
+import type { LoginTabType } from '~/types/definitions/auth.types';
+
+const WithEmail = defineAsyncComponent(() => import('~/components/pages/Auth/Login/WithEmail.vue'));
+const WithMobile = defineAsyncComponent(() => import('~/components/pages/Auth/Login/WithMobile.vue'));
+const WithQRCode = defineAsyncComponent(() => import('~/components/pages/Auth/Login/WithQRCode.vue'));
 
 definePageMeta({
 	layout: 'auth',
 	middleware: 'no-auth',
 });
+
+const loginStore = useLoginStore();
+
+const onChange = (index: number) => {
+	const item = items[index];
+	loginStore.selectedTabLoginType = item.label as LoginTabType;
+};
+
+const items = [
+	{
+		key: 'phoneNumber',
+		label: 'phoneNumber',
+		content: 'This is the content shown for Tab1',
+	},
+	{
+		key: 'email',
+		label: 'email',
+		content: 'And, this is the content for Tab2',
+	},
+	{
+		key: 'qrCode',
+		label: 'qrCode',
+		content: 'Finally, this is the content for Tab3',
+	},
+];
 </script>
--- pages/auth/set-password.vue
+++ pages/auth/set-password.vue
@@ -1,14 +1,104 @@
 <template>
-	<div>
-		<SetPassword />
-	</div>
+	<Auth title="createPassword">
+		<div>
+			<div>
+				<p class="text-sm mb-6 mt-4">
+					{{ $t('enterNewPassword') }}
+				</p>
+			</div>
+			<div class="my-8">
+				<FormsFieldInput
+					id="password"
+					v-model="dto.newPassword"
+					type="password"
+					input-class="text-left"
+					label="newPassword"
+					placeholder=""
+					icon="i-heroicons-eye"
+					dir="ltr"
+					:error-message="
+						v$.newPassword.$error
+							? (v$.newPassword.required.$response ? $t('passwordMustBeComplex') : $t('fieldIsRequired'))
+							: ''
+					"
+				/>
+			</div>
+			<div class="my-8">
+				<FormsFieldInput
+					id="rePassword"
+					v-model="dto.rePassword"
+					type="password"
+					input-class="text-left"
+					label="rePassword"
+					placeholder=""
+					icon="i-heroicons-eye"
+					dir="ltr"
+					:error-message="
+						v$.rePassword.$error
+							? (v$.rePassword.required.$response ? $t('passwordMustBeComplex') : $t('fieldIsRequired'))
+							: ''
+					"
+				/>
+			</div>
+			<div>
+				<UButton
+					size="lg"
+					block
+					:loading="forgetPasswordStore.resetPasswordLoading || localLoading"
+					@click="submit"
+				>
+					{{ $t('confirm') }}
+				</UButton>
+			</div>
+		</div>
+	</Auth>
 </template>
 
-<script setup>
-import SetPassword from '~/components/pages/Auth/SetPassword/index.vue';
+<script setup lang="ts">
+import useVuelidate from '@vuelidate/core';
+
+import Auth from '~/components/pages/Auth/Auth.vue';
+import { complexPassword } from '~/utils/validation-rules';
 
 definePageMeta({
 	layout: 'auth',
 	// middleware: 'no-auth',
 });
+
+const forgetPasswordStore = useForgetPasswordStore();
+
+const router = useRouter();
+
+const localLoading = ref<boolean>(false);
+const dto = ref({
+	newPassword: '',
+	rePassword: '',
+});
+
+const resetPasswordDtoRules = {
+	newPassword: { required: validations.required, complexPassword },
+	rePassword: { required: validations.required, complexPassword },
+};
+
+const v$ = useVuelidate(resetPasswordDtoRules, dto.value);
+
+const submit = async () => {
+	try {
+		v$.value.$touch();
+		if (v$.value.$invalid) {
+			return;
+		}
+		localLoading.value = true;
+		const statusCode = await forgetPasswordStore.resetPassword(dto.value.newPassword);
+
+		if (statusCode === 200) {
+			router.push('/auth/login');
+			forgetPasswordStore.resetAllData();
+		}
+	}
+	catch (error) {
+		localLoading.value = false;
+		console.error('Failed:', error);
+	}
+};
 </script>
